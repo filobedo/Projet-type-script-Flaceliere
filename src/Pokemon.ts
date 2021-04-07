cbf0586a1db52bee9f4de8913060a70a3571c40e
Fred <frederic.Flaceliere@gmail.com>
2021-04-07 16:47:59 +0200

    exo 4 - 5

--- src/Pokemon.ts
+++ src/Pokemon.ts
@@ -1,15 +1,45 @@
 export class Pokemon {
     name: string;
     speed: number;
+    attack = [
+        {
+            title: "pisto cornichon",
+            damage: 20
+        },
+        {
+            title: "pisto crevette",
+            damage: 5
+        },
+        {
+            title: "pisto cnacki",
+            damage: 50
+        },
+    ];
+    life: number;
 
-    constructor(name: string, speed: number) {
+
+    constructor(name: string, speed: number, life: number) {
         this.name = name;
         this.speed = speed;
+        this.life = life;
     }
 
-    fight(pok:Pokemon) {
+    whoAttackFirst(pok: Pokemon): Pokemon {
         if (pok.speed > this.speed)
             return pok;
         return this;
+    }
+
+    fight(dammage: number): Pokemon {
+        this.life -= dammage;
+        if (this.life < 0) {
+            this.life = 0;
+        }
+        return this;
+    }
+
+    choiceAttack(choice: number): number {
+        console.log(`${this.name} choses ${this.attack[choice].title} who deals ${this.attack[choice].damage} damage(s)`);
+        return this.attack[choice].damage;
     }
 }
--- test/Pokemon.test.ts
+++ test/Pokemon.test.ts
@@ -1,10 +1,29 @@
 import {Pokemon} from '../src/Pokemon';
 
-describe("test1",() => {
-   it('mon test', () => {
-       const pika = new Pokemon("toto", 100);
-       const cara = new Pokemon("cara", 100);
-       const res = pika.fight(cara);
+describe("compare between two pokemon",() => {
+   it('should return pokemon one', () => {
+       const pika = new Pokemon("toto", 100, 300);
+       const cara = new Pokemon("cara", 100, 310);
+       const res = pika.whoAttackFirst(cara);
        expect(res.name).toBe("toto");
    });
+
+
+    it('The life of the pokemon Cara going down to 0', () => {
+        const pika = new Pokemon("toto", 100, 300);
+        const cara = new Pokemon("cara", 100, 310);
+
+        while (pika.life > 0 && cara.life > 0) {
+            if (pika.whoAttackFirst(cara) == pika) {
+                pika.fight(pika.choiceAttack(1));
+                cara.fight(cara.choiceAttack(2));
+            } else {
+                cara.fight(cara.choiceAttack(2));
+                pika.fight(pika.choiceAttack(1));
+            }
+        }
+
+        expect(cara.life).toBe(0);
+
+    });
 });
